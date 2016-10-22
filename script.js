4d894bd7d363f76524fd22af010e9e8eafedef0d
Alexey <avstdn@gmail.com>
2016-10-22 08:51:15 +0300

    commit

--- script.js
+++ script.js
@@ -24,41 +24,100 @@ for(var i = 0; i < 6; i++) {
 }
 
 ////////////////////////////////////////////////////////////////////
+///////////////// ERROR FUNCTION ///////////////////////////////////
+////////////////////////////////////////////////////////////////////
+function erf(x) {
+		// constants
+		var a1 =  0.254829592;
+		var a2 = -0.284496736;
+		var a3 =  1.421413741;
+		var a4 = -1.453152027;
+		var a5 =  1.061405429;
+		var p  =  0.3275911;
+		// Save the sign of x
+		var sign = 1;
+		if (x < 0) {
+				sign = -1;
+		}
+		x = Math.abs(x);
+		// A&S formula 7.1.26
+		var t = 1.0/(1.0 + p*x);
+		var y = 1 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*Math.exp(-x*x);
+		return sign*y;
+}
+
+////////////////////////////////////////////////////////////////////
 ///////////////// ACTION BUTTON ////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
 var button = document.getElementById("action");
-button.onclick = function() {
-	var i = 0;
-	var timerId = setInterval(function() {
-	var elements = document.querySelectorAll(".inputColumn li");
-	var elt = elements[i];
-	if(elt.className == "criterion" || elt.className == "headerRow") {
-		i++;
-	}
-	else {
-		elt.className = 'animated rubberBand';
-		i++;
-	}
-	if(i == elements.length) {
-		clearInterval(timerId);
-		console.log("begin!!!");
-	}
-}, 30);
+var inp = document.querySelectorAll(".inp");
+var crit7 = document.querySelector(".crit7");
+var inpArr = new Array(15);
+var bitStr = 0;
+var bitInt = 0;
+var bitSum = 0;
+var criterion7 = 0;
 
-	function func() {
-		var elm = document.querySelectorAll(".inputColumn li");
-		for(var j = 0; j < elm.length; j++) {
-			var el = elm[j];
-			if(el.className == "criterion" || el.className == "headerRow") {
-				continue;
-			}
-			else {
-				el.className = "";
-			}
+button.onclick = function() {
+// 	var i = 0;
+// 	var timerId = setInterval(function() {
+// 	var elements = document.querySelectorAll(".inputColumn li");
+// 	var elt = elements[i];
+// 	if(elt.className == "criterion" || elt.className == "headerRow") {
+// 		i++;
+// 	}
+// 	else {
+// 		elt.className = 'animated rubberBand';
+// 		i++;
+// 	}
+// 	if(i == elements.length) {
+// 		clearInterval(timerId);
+// 	}
+// }, 30);
+
+// 	function func() {
+// 		var elm = document.querySelectorAll(".inputColumn li");
+// 		for(var j = 0; j < elm.length; j++) {
+// 			var el = elm[j];
+// 			if(el.className == "criterion" || el.className == "headerRow") {
+// 				continue;
+// 			}
+// 			else {
+// 				el.className = "";
+// 			}
+// 		}
+// 	}
+// 	setTimeout(func, 1200);
+
+	bitInt = 0;
+	bitStr = "";
+	for(var i = 0; i < 15; i++) {
+		bitInt = parseInt(inp[i].value, 10);
+		// console.log(bitInt);
+		bitStr += (bitInt).toString(2);
+
+		for(var j = 0; j < bitStr.length; j++) {
+			// console.log(bitStr[j]);
 		}
 	}
-	setTimeout(func, 1200);
-}
+	console.log("bit's string:", bitStr);
+
+	for(var i = 0, bitSum = 0; i < bitStr.length; i++) {
+		if(bitStr[i] == "1") {
+			bitSum += 1;
+		}
+		else {
+			bitSum -= 1;
+		}
+	}
+	bitSum = Math.abs(bitSum);
+	console.log("bitSum :", bitSum);
+	console.log("str.length", bitStr.length);
+	console.log("sqrt", Math.sqrt(2 * bitStr.length));
+	crit7 = erf(bitSum / (Math.sqrt(2 * bitStr.length)));
+	console.log(crit7);
+
+} // END OF FUNCTION /////////////
 
 
 ////////////////////////////////////////////////////////////////////
@@ -173,29 +232,6 @@ $('.inputColumn input').bind("change keyup input click", function() {
 });
 
 ////////////////////////////////////////////////////////////////////
-///////////////// ERROR FUNCTION ///////////////////////////////////
-////////////////////////////////////////////////////////////////////
-function erf(x) {
-    // constants
-    var a1 =  0.254829592;
-    var a2 = -0.284496736;
-    var a3 =  1.421413741;
-    var a4 = -1.453152027;
-    var a5 =  1.061405429;
-    var p  =  0.3275911;
-    // Save the sign of x
-    var sign = 1;
-    if (x < 0) {
-        sign = -1;
-    }
-    x = Math.abs(x);
-    // A&S formula 7.1.26
-    var t = 1.0/(1.0 + p*x);
-    var y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*Math.exp(-x*x);
-    return sign*y;
-}
-
-////////////////////////////////////////////////////////////////////
 ////////////////// Lagged Fibonacci Generator //////////////////////
 ////////////////////// GENERATE BUTTON /////////////////////////////
 ////////////////////////////////////////////////////////////////////
@@ -204,22 +240,12 @@ var b = 1;
 var fourthColumnLi = document.querySelectorAll(".fourthColumn li");
 var fifthColumnLi = document.querySelectorAll(".fifthColumn li");
 var sixthColumnLi = document.querySelectorAll(".sixthColumn li");
+var inputColumnLi = document.querySelectorAll('.inputColumn li')
 var generate = document.getElementById("generate");
 var index1 = new Array(10);
 var index2 = new Array(90);
 var index3 = new Array(900);
-var criterion1 = 0;
-var criterion2 = 0;
-var criterion3 = 0;
-var sum = 0;
-var mean = 0;
-var expValue = 0;
-var stdDev = 0;
-var varFactor = 0;
-var inputColumnLi = document.querySelectorAll('.inputColumn li')
-var inp = $("input[class='inp']");
-var inputArr = [];
-var index1 = [];
+
 generate.onclick = function() {
 	var mouseArr = [];
 	var k1 = new Array(4000);
@@ -258,9 +284,7 @@ generate.onclick = function() {
 		}
 		k2[i] = (k1[j]).toString() + (k1[j+1]).toString();
 	}
-	// for(var i = 0; i < k2.length; i++) {
-	//   k2[i] = k2[i] * 100;
-	// }
+
 	for(var i = 1; i <= 1000; i++) {
 		fifthColumnLi[i].innerHTML = k2[i];
 	}
@@ -280,173 +304,66 @@ for(var i = 0, j = 4; i <= 1000; i++, j += 3) {
 ////////////////////////////// ANALYSIS //////////////////////////////////
 ////////////////////////////// CRITERION /////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
-  var criter = 0;
-  for(var i = 0; i < 10; i++) { // FIRST FIB ROW ///////////
-   index1[i] = 0;
-   for(var j = 1; j <= 1000; j++) {
-     if(i == fourthColumnLi[j].innerHTML) {
-       index1[i] += 1;
-     }
-    }
-    if(index1[i] >= 100) {
-      criter += (index1[i] - 100) / 10;
-    }
-    else {
-      criter += (100 - index1[i]) / 10;
-    }
-  }
-  criter = 100 - criter;
-  console.log('index =', index1);
-  console.log('criter =', criter, '%');
+	var criter = 0;
+	for(var i = 0; i < 10; i++) { // FIRST FIB ROW ///////////
+	 index1[i] = 0;
+	 for(var j = 1; j <= 1000; j++) {
+		 if(i == fourthColumnLi[j].innerHTML) {
+			 index1[i] += 1;
+		 }
+		}
+		if(index1[i] >= 100) {
+			criter += (index1[i] - 100) / 10;
+		}
+		else {
+			criter += (100 - index1[i]) / 10;
+		}
+	}
+	criter = 100 - criter;
+	console.log('index =', index1);
+	console.log('criter =', criter, '%');
 
-  var criter = 0;
-  for(var i = 10; i < 99; i++) { // SECOND FIB ROW ///////////
-   index2[i] = 0;
-   for(var j = 1; j <= 1000; j++) {
-     if(i == fifthColumnLi[j].innerHTML) {
-       index2[i] += 1;
-     }
-    }
-    if(index2[i] >= (100/9)) {
-      criter += (index2[i] - (100/9)) / 10;
-    }
-    else {
-      criter += ((100/9) - index2[i]) / 10;
-    }
-  }
-  criter = 100 - criter;
-  console.log('index =', index2);
-  console.log('criter =', criter, '%');
+	var criter = 0;
+	for(var i = 10; i < 99; i++) { // SECOND FIB ROW ///////////
+	 index2[i] = 0;
+	 for(var j = 1; j <= 1000; j++) {
+		 if(i == fifthColumnLi[j].innerHTML) {
+			 index2[i] += 1;
+		 }
+		}
+		if(index2[i] >= (100/9)) {
+			criter += (index2[i] - (100/9)) / 10;
+		}
+		else {
+			criter += ((100/9) - index2[i]) / 10;
+		}
+	}
+	criter = 100 - criter;
+	console.log('index =', index2);
+	console.log('criter =', criter, '%');
 
-	// for(var i = 0; i < 10; i++) { // FIRST FIB ROW ///////////
-	// 	index1[i] = 0;
-	// 	for(var j = 1; j < fourthColumnLi.length - 1; j++) {
-	// 		if(i == fourthColumnLi[j].innerHTML) {
-	// 			index1[i] += 1;
-	// 		}
-	// 		summary += fourthColumnLi.innerHTML;
-	// 	}
-	// 	if(index1[i] >= 100) {
-	// 		criterion1 += (index1[i] - 100) / 10;
-	// 	}
-	// 	else {
-	// 		criterion1 += (100 - index1[i]) / 10;
-	// 	}
-	// }
-
+var test = 1 - erf(2.5996);
+console.log("test:", test);
 
 // TABLE ROW ////////////////////////////////////////////////////
 var criter = 0;
-  for(var i = 0; i < 10; i++) { // FIRST FIB ROW ///////////
-   index1[i] = 0;
-   for(var j = 1; j <= 1000; j++) {
-     if(i == firstColumnLi[j].innerHTML) {
-       index1[i] += 1;
-     }
-    }
-    if(index1[i] >= 100) {
-      criter += (index1[i] - 100) / 10;
-    }
-    else {
-      criter += (100 - index1[i]) / 10;
-    }
-   // if(index1[i] >= 100) {
-   //   criterion1 += (index1[i] - 100) / 10;
-   // }
-   // else {
-   //   criterion1 += (100 - index1[i]) / 10;
-   // }
-  }
-  criter = 100 - criter;
-  console.log('index table =', index1);
-  console.log('criter table =', criter, '%');
-
-
-	// for(var i = 10; i < 100; i++) { // SECOND FIB ROW ////////////
-	// 	index2[i] = 0;
-	// 	for(var j = 1; j < fourthColumnLi.length; j++) {
-	// 		if(i == fifthColumnLi[j].innerHTML) {
-	// 			index2[i] += 1;
-	// 		}
-	// 	}
-	// 	if(index2[i] >= 11.111) {
-	// 		criterion2 += (index2[i] - 11.111) / 90;
-	// 	}
-	// 	else {
-	// 		criterion2 += (11.111 - index2[i]) / 90;
-	// 	}
-	// }
-
-	// for(var i = 100; i < 1000; i++) { // THIRD FIB ROW ///////////////
-	// 	index3[i] = 0;
-	// 	for(var j = 1; j < sixthColumnLi.length; j++) {
-	// 		if(i == sixthColumnLi[j].innerHTML) {
-	// 			index3[i] += 1;
-
-	// 		}
-	// 	}
-	// 	if(index3[i] >= 1.111) {
-	// 		criterion3 += (index3[i] - 1.111) / 900;
-	// 	}
-	// 	else {
-	// 		criterion3 += (1.11 - index3[i]) / 900;
-	// 	}
-	// }
-
-	// console.log(criterion1);
-	// console.log(index1);
-	// console.log(criterion2);
-	// console.log(index2);
-	// console.log(criterion3);
-	// console.log(index3);
-	// criterion1 = Math.floor(100 - criterion1);
-	// criterion2 = Math.floor(100 - criterion2);
-	// criterion3 = Math.floor(100 - criterion3);
-	// var crit4 = document.querySelector('.crit4');
-	// var crit5 = document.querySelector('.crit5');
-	// var crit6 = document.querySelector('.crit6');
-	// crit4.innerHTML = criterion1;
-	// crit5.innerHTML = criterion2;
-	// crit6.innerHTML = criterion3;
-
-	// var count = 0;
-	// for(var i = 0; i < index3.length; i++) {
-	// 	if(index3[i] == 0) {
-	// 		count++;
-	// 	}
-	// }
-	// console.log(count);
- // END OF FUNCTION ///////////////////////////////////////
-sum = 0;
-mean = 0;
-expValue = 0;
-stdDev = 0;
-varFactor = 0;
-
-    inp.each(function() {
-      inputArr.push(this.value);
-    });
-  console.log("input array:", inputArr);
-
-    for(var i = 0; i < 15; i++) { // MEAN /////////////////////////////////
-    sum += parseInt(inputArr[i], 10);
-    if(i == 14) {
-      mean = sum / i;
-      console.log("mean", mean);
-    }
-  }
-  for(var i = 0; i < 15; i++) { // EXPECTATION VALUE ///////////////////
-    expValue += Math.pow((((parseInt(inputArr[i], 10)) - mean) / 15), 2);
-  }
-  console.log("expectation value", expValue);
-  
-  stdDev = Math.sqrt(expValue); // STANDART DEVIATION ////////////////////
-  console.log("standart deviation", stdDev);
-  
-  varFactor = (stdDev / mean) * 100;
-  console.log("variation factor", varFactor, "%"); // VARIATION FACTOR /////////////////////////
-
-
+	for(var i = 0; i < 10; i++) {
+	 index1[i] = 0;
+	 for(var j = 1; j <= 1000; j++) {
+		 if(i == firstColumnLi[j].innerHTML) {
+			 index1[i] += 1;
+		 }
+		}
+		if(index1[i] >= 100) {
+			criter += (index1[i] - 100) / 10;
+		}
+		else {
+			criter += (100 - index1[i]) / 10;
+		}
+	}
+	criter = 100 - criter;
+	console.log('index table =', index1);
+	console.log('criter table =', criter, '%');
 }
 
 ////////////////////////////////////////////////////////////////////
