15ee48a1ff0ea375dbc1f8bb1ceeaba8c8c40248
alexey <avstdn@gmail.com>
2016-10-27 00:22:19 +0300

    commit

--- lab2/script.js
+++ lab2/script.js
@@ -1,11 +1,26 @@
 
-$(function(){
-	$(window).scroll(function() {
-		if($(this).scrollTop() >= 300) {
-		$('panel').addClass('stick-panel');
-		}
-		else{
-			$('panel').removeClass('stick-panel');
-		}
-	});
+// $(function(){
+// 	$(window).scroll(function() {
+// 		if($(this).scrollTop() >= 300) {
+// 		$('panel').addClass('stick-panel');
+// 		}
+// 		else{
+// 			$('panel').removeClass('stick-panel');
+// 		}
+// 	});
+// });
+
+
+$(function() {
+		var box = $('.panel'); // float-fixed block
+		var top = box.offset().top - parseFloat(box.css('marginTop').replace(/auto/, 0));
+		$(window).scroll(function(){
+				var windowpos = $(window).scrollTop();
+				if(windowpos < top) {
+						box.css('position', 'static');
+				} else {
+						box.css('position', 'fixed');
+						box.css('top', 0);
+				}
+		});
 });
