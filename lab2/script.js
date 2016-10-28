39aa4b4830334a4c362e69f702afdb223bf9a982
alexey <avstdn@gmail.com>
2016-10-28 18:09:52 +0300

    commit

--- lab2/script.js
+++ lab2/script.js
@@ -27,26 +27,34 @@
 // 		});
 // });
 
-$(window).scroll(function() {
-	/*----------------------------------
-	sticky block script by makeasite.ru
-	----------------------------------*/
-	var sb_m = 10; /* отступ сверху и снизу */
-	var mb = 300; /* высота подвала с запасом */
-	var st = $(window).scrollTop();
-	var sb = $(".panel");
-	var sbi = $(".panel .inner-panel");
-	var sb_ot = sb.offset().top;
-	var sbi_ot = sbi.offset().top;
-	var sb_h = sb.height();
+// $(window).scroll(function() {
+// 	/*----------------------------------
+// 	sticky block script by makeasite.ru
+// 	----------------------------------*/
+// 	var sb_m = 20; /* отступ сверху и снизу */
+// 	var mb = 300; /* высота подвала с запасом */
+// 	var st = $(window).scrollTop();
+// 	var sb = $(".panel");
+// 	var sbi = $(".panel .inner-panel");
+// 	var sb_ot = sb.offset().top;
+// 	var sbi_ot = sbi.offset().top;
+// 	var sb_h = sb.height();
  
-	if(sb_h + $(document).scrollTop() + sb_m + mb < $(document).height()) {
-		if(st > sb_ot) {
-			var h = Math.round(st - sb_ot) + sb_m;
-			sb.css({"paddingTop" : h});
-		}
-		else {
-			sb.css({"paddingTop" : 10});
-		}
-	}
+// 	if(sb_h + $(document).scrollTop() + sb_m + mb < $(document).height()) {
+// 		if(st > sb_ot) {
+// 			var h = Math.round(st - sb_ot) + sb_m;
+// 			sb.css({"paddingTop" : h});
+// 		}
+// 		else {
+// 			sb.css({"paddingTop" : 20});
+// 		}
+// 	}
+// });
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 200) {
+        $('.panel').addClass('fixed');
+    } else {
+        $('.panel').removeClass('fixed');
+    }
 });
