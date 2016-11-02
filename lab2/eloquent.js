c26842150dd6865825c134acd260e7f6d39bb570
alexey <avstdn@gmail.com>
2016-11-02 16:46:46 +0300

    commit

---
+++ lab2/eloquent.js
@@ -0,0 +1,15 @@
+
+/* FIND MINIMUM*/
+var min = function(a, b) {
+  if(b != undefined) {
+    if(a > b) {
+      return b;
+    }
+    else {
+      return b;
+    }
+  }
+  else {
+    console.log("error");
+  }
+}
