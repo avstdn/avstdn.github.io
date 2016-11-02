cbf42d89c953275011d58e0730874df241d1f9d6
alexey <avstdn@gmail.com>
2016-11-02 16:49:05 +0300

    commit

--- lab2/script.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load', function () {
-  $('.wrapper').delay(0).fadeOut();
-});
-
-$(document).ready(function(){
-  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-    $(this).toggleClass('open');
-  });
-});
