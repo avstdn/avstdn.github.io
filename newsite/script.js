d9e929336e49b5386e8d4d7639af1acc8f81dbf5
alexey <avstdn@gmail.com>
2016-11-05 01:00:23 +0300

    commit

--- newsite/script.js
+++ newsite/script.js
@@ -1,6 +1,16 @@
 // BURGER OPEN //////////////
+var toggle = true;
 $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
     $(this).toggleClass('open');
     $('.wrapper').toggleClass('wrapper-faded');
+    if(toggle) {
+      $('.faded-menu').removeClass('animated fadeOutLeft');
+      $('.faded-menu').addClass('animated bounceInLeft');
+      toggle = false;
+    } else {
+      $('.faded-menu').removeClass('animated bounceInLeft');
+      $('.faded-menu').addClass('animated fadeOutLeft');
+      toggle = true;
+    }
 });
 
