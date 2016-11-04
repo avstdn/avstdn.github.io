868bd06557d990ab3f1c92e4bf4401a616c45a2a
alexey <avstdn@gmail.com>
2016-11-04 23:32:28 +0300

    commit

---
+++ newsite/script.js
@@ -0,0 +1,6 @@
+// BURGER OPEN //////////////
+$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+    $(this).toggleClass('open');
+    $('.wrapper').toggleClass('wrapper-faded');
+});
+
