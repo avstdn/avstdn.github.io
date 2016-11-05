b59b5ffeb08ed82f87a911e592ddf793f3dd8075
alexey <avstdn@gmail.com>
2016-11-05 18:22:15 +0300

    commit

--- newsite/script.js
+++ newsite/script.js
@@ -16,32 +16,32 @@ $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 
 // SCROLL TO SECTION ////////
 $(document).ready(function(){
-  $('.dot1').click(function(){
-    $('[class ^= "dot"]').css('background', '#666');
+  $('#1').click(function(){
+    // $('[class ^= "dot"]').css('background', '#666');
     var speed = 1000; // scrolling speed
     var top = $('.first-section').offset().top; // scroll to
     // $(this).css('background', '#fff');
     $('html, body').animate({scrollTop: top}, speed);
     return false;
   });
-  $('.dot2').click(function(){
-    $('[class ^= "dot"]').css('background', '#666');
+  $('#2').click(function(){
+    // $('[class ^= "dot"]').css('background', '#666');
     var speed = 1000;
     // $(this).css('background', '#fff');
     var top = $('.second-section').offset().top;
     $('html, body').animate({scrollTop: top}, speed);
     return false;
   });
-  $('.dot3').click(function(){
-    $('[class ^= "dot"]').css('background', '#666');
+  $('#3').click(function(){
+    // $('[class ^= "dot"]').css('background', '#666');
     var speed = 1000;
     // $(this).css('background', '#fff');
     var top = $('.third-section').offset().top;
     $('html, body').animate({scrollTop: top}, speed);
     return false;
   });
-  $('.dot4').click(function(){
-    $('[class ^= "dot"]').css('background', '#666');
+  $('#4').click(function(){
+    // $('[class ^= "dot"]').css('background', '#666');
     var speed = 1000;
     // $(this).css('background', '#fff');
     var top = $('.footer').offset().top;
@@ -61,22 +61,22 @@ $(window).on('scroll', function () {
   var top = $('.first-section').offset().top;
   var bottom = top + $('.first-section').outerHeight();
     if (curPos >= top && curPos <= bottom) {
-      $('[class ^= "dot"]').css('background', '#666');
-      $('.dot1').css('background', '#fff');
+      $('.dot').removeClass('dot-active');
+      $('#1 .dot').addClass('dot-active');
     }
 
   var top = $('.second-section').offset().top;
   var bottom = top + $('.second-section').outerHeight();
     if (curPos >= top && curPos <= bottom) {
-      $('[class ^= "dot"]').css('background', '#666');
-      $('.dot2').css('background', '#fff');
+      $('.dot').removeClass('dot-active');
+      $('#2 .dot').addClass('dot-active');
     }
 
   var top = $('.third-section').offset().top;
   var bottom = top + $('.third-section').outerHeight();
     if (curPos >= top && curPos <= bottom) {
-      $('[class ^= "dot"]').css('background', '#666');
-      $('.dot3').css('background', '#fff');
+      $('.dot').removeClass('dot-active');
+      $('#3 .dot').addClass('dot-active');
     }
 
 });
