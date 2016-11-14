f614318d7c0b3b9e07a77f6a9ff4b03c4679fd7f
alexey <avstdn@gmail.com>
2016-11-14 14:50:26 +0300

    commit

--- qs/script.js
+++ qs/script.js
@@ -1,9 +1,8 @@
 
+var button2 = document.querySelector("button2");
 
-var button = document.querySelector("button");
 
-addEventListener("click", function() {
-
+if(button2) {
 if (window.Worker) { // Check if Browser supports the Worker api.
   // Requries script name as input
   var worker1 = new Worker("worker1.js");
@@ -14,7 +13,7 @@ if (window.Worker) { // Check if Browser supports the Worker api.
     // worker2.postMessage('message from main stream to worker2');
 
   var test = function() {
-      worker1.postMessage('message from main stream to worker1');
+      // worker1.postMessage('message from main stream to worker1');
       console.log('Message posted to workers');
   }
 
@@ -28,17 +27,17 @@ if (window.Worker) { // Check if Browser supports the Worker api.
   //   console.log(e.data);
   // };
 }
-});
+}
 
 var counter = 0;
 var fill = function() {
   if(counter <= 100) {
-  $('.bar-fill').css('width', counter + '%');
+  // $('.bar-fill').css('width', counter + '%');
   counter += 1;
-  console.log(counter);
+  // console.log(counter);
   } else {
     counter = 0;
-    $('.bar-fill').css('width', counter + '%');
+    // $('.bar-fill').css('width', counter + '%');
   }
 }
 
@@ -49,7 +48,7 @@ var fill2 = function() {
   if(counter2 <= 100) {
   $('.bar-fill2').css('width', counter2 + '%');
   counter2 += 1;
-  console.log(counter2);
+  // console.log(counter2);
   }
   else {
     counter2 = 0;
@@ -57,6 +56,58 @@ var fill2 = function() {
 }
 
 setInterval(fill2, 100);
+
+
+
+/////////////////////////
+
+var request = 0;
+
+
+var queue = [
+              { workerQueue: 0, isBusy: false , totalRequest: 0 },
+              { workerQueue: 0, isBusy: false , totalRequest: 0 },
+              { workerQueue: 0, isBusy: false , totalRequest: 0 },
+              { workerQueue: 0, isBusy: false , totalRequest: 0 },
+              { workerQueue: 0, isBusy: false , totalRequest: 0 },
+            ];
+
+
+function findMin() {
+  var min = queue[0].workerQueue;
+  var indexOfMin = 0;
+  for (var i = 0; i < 4; i++) {
+    if(min > queue[i+1].workerQueue) {
+      indexOfMin = i + 1;
+      min = queue[i+1].workerQueue;
+    }
+  }
+  queue[indexOfMin].workerQueue += 1;
+  request -= 1;
+}
+
+function showWorkerQueue() {
+  document.querySelector('#queue-1').innerHTML = queue[0].workerQueue;
+  document.querySelector('#queue-2').innerHTML = queue[1].workerQueue;
+  document.querySelector('#queue-3').innerHTML = queue[2].workerQueue;
+  document.querySelector('#queue-4').innerHTML = queue[3].workerQueue;
+  document.querySelector('#queue-5').innerHTML = queue[4].workerQueue;
+}
+
+var requestTab = document.querySelector('.request-tab');
+
+function createRequest() {
+  request += 1;
+  requestTab.innerHTML = request;
+  findMin();
+  showWorkerQueue();
+}
+
+var button = document.querySelector("#button");
+button.addEventListener("click", function() {
+setInterval(createRequest, 500);
+})
+
 
 
 
