a01c3c2eb2c16817c07f7d1cfec3c5bea16f4667
alexey <avstdn@gmail.com>
2016-11-18 19:13:53 +0300

    commit

--- qs/script.js
+++ qs/script.js
@@ -31,6 +31,12 @@ $('#countdown-3').text('READY').css('background-color', '#68E768');
 $('#countdown-4').text('READY').css('background-color', '#68E768');
 $('#countdown-5').text('READY').css('background-color', '#68E768');
 
+var input = document.querySelector("#speed");
+var speed = 200;
+input.oninput = function() {
+   speed = input.value;
+}
+
 var generate = document.querySelector("#generate");
 generate.addEventListener("click", function() {
   createRequest();
@@ -38,11 +44,12 @@ generate.addEventListener("click", function() {
 var start = document.querySelector("#start");
 start.addEventListener("click", function() {
   createRequest();
-  setInterval(createRequest, 200);
+  setInterval(createRequest, input.value);
 });
 
 function createRequest() {
   var minimum = 0;
+  console.log('speed', speed);
   request++;
   document.querySelector(".request-tab").innerHTML = request;
   document.querySelector("#total-req").innerHTML = request;
