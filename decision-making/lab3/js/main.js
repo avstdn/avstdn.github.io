853a478a91711ad869b501f7e48ba26b9029b9c0
Alexey <avstdn@gmail.com>
2017-02-10 00:50:40 +0300

    commit

--- decision-making/lab3/js/main.js
+++ decision-making/lab3/js/main.js
@@ -1,14 +1,14 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-var radius = 25;
+var radius = 7;
 
 canvas.width = window.innerWidth;
 canvas.height = 500;
 
 var putPoint = function(e) {
   context.beginPath();
-  context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2);
+  context.arc(e.offsetX, e.offsetY, radius/2, 0, Math.PI*2);
   context.fill();
 }
 
