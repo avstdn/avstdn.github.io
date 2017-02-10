699bdbc74df40cbab02391c777e0822b4d8004a4
Alexey <avstdn@gmail.com>
2017-02-10 22:43:21 +0300

    commit

--- decision-making/lab3/js/main.js
+++ decision-making/lab3/js/main.js
@@ -10,7 +10,10 @@ var putPoint = function(e) {
   context.beginPath();
   context.arc(e.offsetX, e.offsetY, radius/2, 0, Math.PI*2);
   context.fill();
+  coords.innerHTML = 'X: ' + e.offsetX + ' and Y: ' + e.offsetY;
   console.log(e.offsetX, e.offsetY);
 }
 
 canvas.addEventListener('mousedown', putPoint);
+
+var coords = document.getElementById('coords');
