3aaa8ca13e0a2ac5456cd9bf82c0d81a875f0cd3
Alexey <avstdn@gmail.com>
2017-02-13 00:55:36 +0300

    commit

---
+++ future/js/script.js
@@ -0,0 +1,10 @@
+var title = (
+  <div>
+    <h1>Hi everybody!</h1>
+    <h2>This is my first React web application!</h2>
+  </div>
+  );
+
+
+
+ReactDOM.render(title, document.getElementById('root'));
