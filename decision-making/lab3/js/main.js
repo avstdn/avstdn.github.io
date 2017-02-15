b07e5203cf02cf80c8a93ddcbedaeff5c905d3cb
Alexey <avstdn@gmail.com>
2017-02-15 21:34:35 +0300

    commit

--- decision-making/lab3/js/main.js
+++ decision-making/lab3/js/main.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
 var radius = 7;
 
@@ -7,13 +8,22 @@ canvas.width = window.innerWidth - 50;
 canvas.height = 400;
 
 var putPoint = function(e) {
+
+  context.beginPath();
+  context.arc(e.offsetX, e.offsetY, 40, 0, Math.PI*2);
+  context.strokeStyle = 'yellow';
+  context.stroke();
+  context.closePath();
+
   context.beginPath();
   context.arc(e.offsetX, e.offsetY, radius/2, 0, Math.PI*2);
+  // coords.innerHTML = 'X: ' + e.offsetX + ' and Y: ' + e.offsetY;
   context.fill();
-  coords.innerHTML = 'X: ' + e.offsetX + ' and Y: ' + e.offsetY;
+  context.closePath();
+
   console.log(e.offsetX, e.offsetY);
 }
 
 canvas.addEventListener('mousedown', putPoint);
 
-var coords = document.getElementById('coords');
+// var coords = document.getElementById('coords');
