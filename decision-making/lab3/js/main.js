6c1cd57f86ade249a8c8d09c3ed1eff87b9fbaa5
Alexey <avstdn@gmail.com>
2017-02-16 03:31:59 +0300

    commit

--- decision-making/lab3/js/main.js
+++ decision-making/lab3/js/main.js
@@ -1,29 +1,101 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-var ctx = canvas.getContext('2d');
+var objectsArray = [];
 
 var radius = 7;
+
+var outerRadius = 40;
 
 canvas.width = window.innerWidth - 50;
 canvas.height = 400;
 
 var putPoint = function(e) {
 
+  addToObjectsArray(e.offsetX, e.offsetY, 'undefined');
+
   context.beginPath();
-  context.arc(e.offsetX, e.offsetY, 40, 0, Math.PI*2);
+  context.arc(e.offsetX, e.offsetY, outerRadius, 0, Math.PI*2);
   context.strokeStyle = 'yellow';
   context.stroke();
   context.closePath();
 
   context.beginPath();
   context.arc(e.offsetX, e.offsetY, radius/2, 0, Math.PI*2);
-  // coords.innerHTML = 'X: ' + e.offsetX + ' and Y: ' + e.offsetY;
+  context.fillStyle = 'black';
   context.fill();
   context.closePath();
+
+  if(objectsArray.length > 0) { // В массиве должно быть больше 2-х элементов
+    for(var i = 0; i < objectsArray.length - 1; i++) { 
+      if(isOwned(objectsArray[i].x, objectsArray[i].y, objectsArray[objectsArray.length-1].x, objectsArray[objectsArray.length-1].y)) { // Если ф-ия вернула true, то объединяем точки в кластер
+        if(setColor(objectsArray[i], objectsArray[objectsArray.length-1])) { // Если ф-ия вернула true, то красим обе точки
+          redrawPoints(objectsArray[i], objectsArray[objectsArray.length-1]);
+        } else { // Иначе, красим только одну точку
+          drawPoint(objectsArray[objectsArray.length-1]);
+        }
+      }
+    }
+  }
 
   console.log(e.offsetX, e.offsetY);
 }
 
 canvas.addEventListener('mousedown', putPoint);
 
-// var coords = document.getElementById('coords');
+
+function addToObjectsArray(coordX, coordY, cluster) {
+  objectsArray.push({x: coordX, y: coordY, color: cluster});
+}
+
+function isOwned(x1, y1, x2, y2) {
+  if(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) <= outerRadius * 2) {
+    console.log('isOwned', true);
+    return true;
+  } else {
+    console.log('isOwned', false);
+    return false;
+  }
+}
+
+function setColor(obj1, obj2) {
+  if(obj1.color == 'undefined') {
+    obj1.color = 'red';
+    obj2.color = 'red';
+    console.log('setColor', true);
+    return true;
+  } else {
+    obj2.color = 'red';
+    console.log('setColor', false);
+    return false;
+  }
+}
+
+function drawPoint(obj2) {
+  context.beginPath();
+  context.fillStyle = obj2.color;
+  context.arc(obj2.x, obj2.y, radius/2, 0, Math.PI*2);
+  context.fill();
+  context.closePath();
+}
+
+function redrawPoints(obj1, obj2) {
+  context.beginPath();
+  context.arc(obj1.x, obj1.y, radius/2, 0, Math.PI*2);
+  context.fillStyle = obj2.color;
+  context.fill();
+  context.closePath();
+  context.beginPath();
+  context.arc(obj2.x, obj2.y, radius/2, 0, Math.PI*2);
+  context.fill();
+  context.closePath();
+}
+
+
+
+  // } else {
+  //   if(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) <= outerRadius * 2) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
--- decision-making/lab3/js/script.js
+++ decision-making/lab3/js/script.js
@@ -115,38 +115,38 @@
 
   /* Awesome burger scaled */
 
-  var pathD = document.getElementById('pathD'),
-    pathE = document.getElementById('pathE'),
-    pathF = document.getElementById('pathF'),
-    segmentD = new Segment(pathD, beginAC, endAC),
-    segmentE = new Segment(pathE, beginB, endB),
-    segmentF = new Segment(pathF, beginAC, endAC),
-    wrapper2 = document.getElementById('menu-icon-wrapper2'),
-    trigger2 = document.getElementById('menu-icon-trigger2'),
-    toCloseIcon2 = true,
-    dummy2 = document.getElementById('dummy2');
+  // var pathD = document.getElementById('pathD'),
+  //   pathE = document.getElementById('pathE'),
+  //   pathF = document.getElementById('pathF'),
+  //   segmentD = new Segment(pathD, beginAC, endAC),
+  //   segmentE = new Segment(pathE, beginB, endB),
+  //   segmentF = new Segment(pathF, beginAC, endAC),
+  //   wrapper2 = document.getElementById('menu-icon-wrapper2'),
+  //   trigger2 = document.getElementById('menu-icon-trigger2'),
+  //   toCloseIcon2 = true,
+  //   dummy2 = document.getElementById('dummy2');
 
-  wrapper2.style.visibility = 'visible';
+  // wrapper2.style.visibility = 'visible';
 
-  trigger2.onclick = function() {
-    addScale(wrapper2);
-    if (toCloseIcon2) {
-      inAC(segmentD);
-      inB(segmentE);
-      inAC(segmentF);
+  // trigger2.onclick = function() {
+  //   addScale(wrapper2);
+  //   if (toCloseIcon2) {
+  //     inAC(segmentD);
+  //     inB(segmentE);
+  //     inAC(segmentF);
 
-      dummy2.className = 'dummy dummy--active';
-    } else {
-      outAC(segmentD);
-      outB(segmentE);
-      outAC(segmentF);
+  //     dummy2.className = 'dummy dummy--active';
+  //   } else {
+  //     outAC(segmentD);
+  //     outB(segmentE);
+  //     outAC(segmentF);
 
-      dummy2.className = 'dummy';
-    }
-    toCloseIcon2 = !toCloseIcon2;
-    setTimeout(function() {
-      removeScale(wrapper2)
-    }, 450);
-  };
+  //     dummy2.className = 'dummy';
+  //   }
+  //   toCloseIcon2 = !toCloseIcon2;
+  //   setTimeout(function() {
+  //     removeScale(wrapper2)
+  //   }, 450);
+  // };
 
 })();
