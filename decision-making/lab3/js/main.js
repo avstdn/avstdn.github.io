e144bb7f7be8cbd3c5ba93095c5308fcf3358ba5
Alexey <avstdn@gmail.com>
2017-02-17 20:56:40 +0300

    commit

--- decision-making/lab3/js/main.js
+++ decision-making/lab3/js/main.js
@@ -1,20 +1,22 @@
 var canvas = document.getElementById('canvas');
+var clear = document.getElementById('clear');
+// var showRadius = document.getElementById('show-radius');
 var context = canvas.getContext('2d');
 var objectsArray = [];
 var colorsArray = [
-                    'teal', 'green', 'blue', 'red', 'pink', 'turquoise', 'orange', 'purple', 'maroon',
-                    'GreenYellow', 'MediumSpringGreen', 'LightCoral', 'DeepPink', 'LightSalmon', 'PeachPuff',
-                    'Aquamarine', 'Magenta', 'Chocolate', 'LightSlateGrey', 'Lime'
+                    'red', 'green', 'blue', 'Aquamarine', 'pink', 'turquoise', 'orange', 'purple', 'PeachPuff',
+                    'GreenYellow', 'MediumSpringGreen', 'LightCoral', 'DeepPink', 'LightSalmon', 'maroon',
+                    'teal', 'Magenta', 'Chocolate', 'LightSlateGrey', 'Lime'
                   ];
 var clr = 0;
 var notReached = false;
-
 var radius = 7;
-
 var outerRadius = 20;
 
 canvas.width = window.innerWidth - 50;
-canvas.height = 400;
+canvas.height = window.innerHeight - 180;
+
+///////////////////////////// MAIN ////////////////////////////////////////////////////
 
 var putPoint = function(e) {
   addToObjectsArray(e.offsetX, e.offsetY, colorsArray[clr % (colorsArray.length - 1)]);
@@ -25,23 +27,27 @@ var putPoint = function(e) {
   // context.stroke();
   // context.closePath();
 
-
-///////////////////////////// MAIN //////////////////////////////////////////////
-  if(objectsArray.length > 1) { // В массиве должно быть больше 2-х элементов
-
+   // В массиве должно быть больше 2-х элементов
+  if(objectsArray.length > 1) {
     notReached = true;
 
     for(var i = objectsArray.length - 2; i >= 0 ; i--) {
+
+      // Находится ли точка с зоне досягаемости
       if(isOwned(objectsArray[i].x, objectsArray[i].y, objectsArray[objectsArray.length-1].x, objectsArray[objectsArray.length-1].y)) { // Если ф-ия вернула true, то точка досигаема
+
+        // Обе точки одинакового цвета
         if(isCurrentColor(objectsArray[i], objectsArray[objectsArray.length-1])) {
           drawPoint(objectsArray[i], objectsArray[objectsArray.length-1]);
-        } else {
-          // Если есть связь с точкой текущего цвета
 
+        // Точки разного цвета
+        } else {
+
+          // Если есть связь с точкой текущего цвета
           if(isReached(objectsArray[objectsArray.length-1])) {
           redrawPoints(objectsArray[i], objectsArray[objectsArray.length-1]);
-          console.log('is not current color');
-          // Если связь только с досягаемыми точками
+
+          // Иначе, есть ли связь только с досягаемыми точками
           } else {
             drawCurrentPoint(objectsArray[i], objectsArray[objectsArray.length-1]);
           }
@@ -49,6 +55,8 @@ var putPoint = function(e) {
         notReached = false;
       }
     }
+
+    // Если текущая точка не касается других точек
     if(notReached) {
       clr++;
       objectsArray[objectsArray.length-1].color = colorsArray[clr % (colorsArray.length - 1)];
@@ -59,7 +67,8 @@ var putPoint = function(e) {
       context.closePath();
     }
 
-  } else { // Если в массиве один элемент
+  // Если в массиве один элемент
+  } else {
     context.beginPath();
     context.arc(e.offsetX, e.offsetY, radius/2, 0, Math.PI*2);
     context.fillStyle = colorsArray[clr % (colorsArray.length - 1)];
@@ -70,6 +79,8 @@ var putPoint = function(e) {
 }
 
 canvas.addEventListener('mousedown', putPoint);
+clear.addEventListener('mousedown', clearCanvas);
+// showRadius.addEventListener('mousedown', putPointWithRadius);
 
 
 function addToObjectsArray(coordX, coordY, cluster) {
@@ -78,10 +89,8 @@ function addToObjectsArray(coordX, coordY, cluster) {
 
 function isOwned(x1, y1, x2, y2) {
   if(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) <= outerRadius * 2) {
-    console.log('isOwned', true);
     return true;
   } else {
-    console.log('isOwned', false);
     return false;
   }
 }
@@ -106,7 +115,6 @@ function isReached(currentPoint) {
 }
 
 function drawPoint(reachedPoint, currentPoint) {
-  // currentPoint.color = reachedPoint.color;
   context.beginPath();
   context.arc(currentPoint.x, currentPoint.y, radius/2, 0, Math.PI*2);
   context.fillStyle = currentPoint.color;
@@ -142,3 +150,78 @@ function redrawPoints(reachedPoint, currentPoint) {
   context.fill();
   context.closePath();
 }
+
+
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  clr = 0;
+  notReached = false;
+  objectsArray = [];
+}
+
+
+
+
+var putPointWithRadius = function(e) {
+  addToObjectsArray(e.offsetX, e.offsetY, colorsArray[clr % (colorsArray.length - 1)]);
+
+  context.beginPath();
+  context.arc(e.offsetX, e.offsetY, outerRadius, 0, Math.PI*2);
+  context.strokeStyle = 'yellow';
+  context.stroke();
+  context.closePath();
+
+   // В массиве должно быть больше 2-х элементов
+  if(objectsArray.length > 1) {
+    notReached = true;
+
+    for(var i = objectsArray.length - 2; i >= 0 ; i--) {
+
+      // Находится ли точка с зоне досягаемости
+      if(isOwned(objectsArray[i].x, objectsArray[i].y, objectsArray[objectsArray.length-1].x, objectsArray[objectsArray.length-1].y)) { // Если ф-ия вернула true, то точка досигаема
+
+        // Обе точки одинакового цвета
+        if(isCurrentColor(objectsArray[i], objectsArray[objectsArray.length-1])) {
+          drawPoint(objectsArray[i], objectsArray[objectsArray.length-1]);
+
+        // Точки разного цвета
+        } else {
+
+          // Если есть связь с точкой текущего цвета
+          if(isReached(objectsArray[objectsArray.length-1])) {
+          redrawPoints(objectsArray[i], objectsArray[objectsArray.length-1]);
+
+          // Иначе, есть ли связь только с досягаемыми точками
+          } else {
+            drawCurrentPoint(objectsArray[i], objectsArray[objectsArray.length-1]);
+          }
+        }
+        notReached = false;
+      }
+    }
+
+    // Если текущая точка не касается других точек
+    if(notReached) {
+      clr++;
+      objectsArray[objectsArray.length-1].color = colorsArray[clr % (colorsArray.length - 1)];
+      context.beginPath();
+      context.arc(e.offsetX, e.offsetY, radius/2, 0, Math.PI*2);
+      context.fillStyle = colorsArray[clr % (colorsArray.length - 1)];
+      context.fill();
+      context.closePath();
+    }
+
+  // Если в массиве один элемент
+  } else {
+    context.beginPath();
+    context.arc(e.offsetX, e.offsetY, radius/2, 0, Math.PI*2);
+    context.fillStyle = colorsArray[clr % (colorsArray.length - 1)];
+    context.fill();
+    context.closePath();
+
+  } ///////////////////////////////////////////////////////////////////////
+}
+
+// ВТ ГЗ  в 12:00, ауд 423с 425с
+
+// ЧТ с 14:00 Числитель - УЛК, в 17:30 в ГЗ
